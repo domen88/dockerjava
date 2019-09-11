3d5ebb91bb669a007fe7faafcffb4c1abcb11f5e
Domenico Scotece <domenico.scotece@unibo.it>
2019-09-11 14:06:10 +0200

    Add clear cache script

--- check-jar.sh
+++ check-jar.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-pwd
+
 if test -f target/$1.jar
 then
     echo "jar exists"
-    ls -l target/
 else 
     echo "jar not exists"
 fi
---
+++ clean-cache.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+rm -f $HOME/.m2/repository/it/unibo/scotece/domenico/docker-java/1.0-SNAPSHOT/maven-metadata-local.xml
+rm -f $HOME/.m2/repository/it/unibo/scotece/domenico/docker-java/1.0-SNAPSHOT/_remote.repositories
+rm -f $HOME/.m2/repository/it/unibo/scotece/domenico/docker-java/maven-metadata-local.xml
